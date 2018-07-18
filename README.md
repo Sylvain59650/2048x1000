954cdfe505b7e118e5c1a5a8c07e963ccbb0ccc2
Gogs <gogs@fake.local>
2018-07-18 10:30:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 2048x1000
+
+You can play here (https://github.io)
+Based on 2048 game created by Gabriele Cirulli.
+Random tiles is not only 2 or 4.
+
+A small clone of [1024](https://play.google.com/store/apps/details?id=com.veewo.a1024), based on [Saming's 2048](http://saming.fr/p/2048/) (also a clone).
---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+﻿const babel = require("gulp-babel");
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const watch = require("gulp-watch");
+let cleanCSS = require("gulp-clean-css");
+
+const chemins = {
+  sources: "./src/",
+  demo: "./docs/demo/"
+};
+
+
+
+gulp.task("2048x1000.min.js", () => {
+  return gulp.src([
+      "src/js/**/*.js"
+    ])
+    .pipe(concat("2048x1000.min.js"))
+    .pipe(babel({
+      presets: ["es2015"],
+      compact: true,
+      comments: false,
+      minified: true
+    }))
+    .pipe(gulp.dest(chemins.demo))
+});
+
+
+
+gulp.task("watch:2048x1000.min.js", function() {
+  watch("./src/js/**.js", function() {
+    gulp.run("2048x1000.min.js");
+  });
+});
+
+
+
+gulp.task("html", () => {
+  return gulp.src(["src/index.html", "meta/*/*", "favicon.png"])
+    .pipe(gulp.dest(chemins.demo));
+});
+
+gulp.task("watch:html", function() {
+  watch("./src/**.html", function() {
+    gulp.run("html");
+  });
+});
+
+gulp.task("2048x1000.min.css", () => {
+  return gulp.src([
+      "./style/*.css"
+    ])
+    .pipe(concat("2048x1000.min.css"))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(chemins.demo));
+});
+
+gulp.task("watch:html", function() {
+  watch("./src/**.html", function() {
+    gulp.run("html");
+  });
+});
+
+gulp.task("watch:2048x1000.min.css", function() {
+  watch("./src/**.css", function() {
+    gulp.run("2048x1000.min.css");
+  });
+});
+
+
+gulp.task("default", ["2048x1000.min.js", "html", "2048x1000.min.css"]);
+
+gulp.task("watch", ["watch:2048x1000.min.js", "watch:html", "watch:2048x1000.min.css"]);
